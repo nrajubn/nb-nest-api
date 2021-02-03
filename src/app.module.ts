8240efbfc97e5ed5bf6fefa78852604abd8986ef
nrajubn <60019513+nrajubn@users.noreply.github.com>
2021-02-02 23:24:01 -0600

    Added typeORM, DB connection and location resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestModule } from './quest/quest.module';
 import { UserModule } from './user/user.module';
+import { LocationModule } from './location/location.module';
 
 @Module({
-  imports: [QuestModule, UserModule],
+  
   controllers: [AppController],
   providers: [AppService],
+
+  imports: [QuestModule, UserModule
+  TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'test',
+    entities: [],
+    synchronize: true,
+  }),
+  LocationModule,
+],
 })
 export class AppModule {}
